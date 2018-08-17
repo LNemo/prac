ded2c0fe1c53c0b7edafd915f68be82570c71c82
LNemo <kkmm0512@naver.com>
2018-08-18 02:08:23 +0900

    bored 2am

---
+++ Practice/src/com/prac/aug_18/bored/MainClass.java
@@ -0,0 +1,32 @@
+package com.prac.aug_18.bored;
+
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = 1;
+		String str;
+		
+		while(n == 1) {
+			
+			System.out.print("대머리는 (사회악 / 무죄) (이)다.: ");
+			str = sc.nextLine();
+			
+			if(str.equals("사회악")) {
+				System.out.println("대머리는 사회악이다!");
+				n = 0;
+			}else if(str.equals("무죄")) {
+				System.out.println("당신, 탈모시군요?");
+				n = 0;
+			}else {
+				System.out.println("주어진 단어를 입력하셔야죠?");
+				System.out.println("");
+			}
+			
+		}
+		
+	}
+	
+}
